c3d68bfb0d9e86cf4dafd31089bc71cc93e99f32
bambademe <cheikhbambademe@gmail.com>
2024-02-05 22:35:46 +0000

    add cors and security configuration

---
+++ src/main/java/deme/ahmadou/inventoryservice/security/SecurityConfig.java
@@ -0,0 +1,45 @@
+package deme.ahmadou.inventoryservice.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(csrf -> csrf.disable()) // disable csrf for h2
+                .cors(Customizer.withDefaults())
+                .headers(h -> h.frameOptions(frameOptionsConfig -> frameOptionsConfig.disable())) // disable frame options config for h2
+                .authorizeHttpRequests(ar -> ar.requestMatchers("/api/v1/**", "/swagger-ui.html","/swagger-ui/**","/v3/**","/h2-console/**").permitAll())
+                .authorizeHttpRequests(ar -> ar.anyRequest().authenticated())
+                .build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource(){
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowedMethods(List.of("*"));
+        configuration.setExposedHeaders(List.of("*"));
+
+
+        UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+        urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", configuration);
+
+        return urlBasedCorsConfigurationSource;
+    }
+}
